50731b156ce3352e85fc9add510da1c20e23142b
abed42 <abedhamami42@gmail.com>
2024-07-04 16:03:41 +0300

    removes another useless comment

--- components/Login.tsx
+++ components/Login.tsx
@@ -145,7 +145,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 48,
     marginTop: 24,
-    backgroundColor: "#0029FF", // Example background color
+    backgroundColor: "#0029FF",
     padding: 12,
     alignItems: "center",
     justifyContent: "center",
