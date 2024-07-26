4fcb85cfdacfcdcb77db5c8212461f853d13a3f9
abed42 <abedhamami42@gmail.com>
2024-07-26 23:09:52 +0300

    updates action btn colors

--- components/Article.tsx
+++ components/Article.tsx
@@ -66,7 +66,7 @@ const LikeSvg = ({ style }: { style: any }) => {
 };
 const DislikeSvg = ({ style }: { style: any }) => {
   return (
-    <Svg style={styles.actionSvg} viewBox="0 0 512 512">
+    <Svg style={style} viewBox="0 0 512 512">
       <Path d="M323.8 477.2c-38.2 10.9-78.1-11.2-89-49.4l-5.7-20c-3.7-13-10.4-25-19.5-35l-51.3-56.4c-8.9-9.8-8.2-25 1.6-33.9s25-8.2 33.9 1.6l51.3 56.4c14.1 15.5 24.4 34 30.1 54.1l5.7 20c3.6 12.7 16.9 20.1 29.7 16.5s20.1-16.9 16.5-29.7l-5.7-20c-5.7-19.9-14.7-38.7-26.6-55.5c-5.2-7.3-5.8-16.9-1.7-24.9s12.3-13 21.3-13L448 288c8.8 0 16-7.2 16-16c0-6.8-4.3-12.7-10.4-15c-7.4-2.8-13-9-14.9-16.7s.1-15.8 5.3-21.7c2.5-2.8 4-6.5 4-10.6c0-7.8-5.6-14.3-13-15.7c-8.2-1.6-15.1-7.3-18-15.2s-1.6-16.7 3.6-23.3c2.1-2.7 3.4-6.1 3.4-9.9c0-6.7-4.2-12.6-10.2-14.9c-11.5-4.5-17.7-16.9-14.4-28.8c.4-1.3 .6-2.8 .6-4.3c0-8.8-7.2-16-16-16l-97.5 0c-12.6 0-25 3.7-35.5 10.7l-61.7 41.1c-11 7.4-25.9 4.4-33.3-6.7s-4.4-25.9 6.7-33.3l61.7-41.1c18.4-12.3 40-18.8 62.1-18.8L384 32c34.7 0 62.9 27.6 64 62c14.6 11.7 24 29.7 24 50c0 4.5-.5 8.8-1.3 13c15.4 11.7 25.3 30.2 25.3 51c0 6.5-1 12.8-2.8 18.7C504.8 238.3 512 254.3 512 272c0 35.3-28.6 64-64 64l-92.3 0c4.7 10.4 8.7 21.2 11.8 32.2l5.7 20c10.9 38.2-11.2 78.1-49.4 89zM32 384c-17.7 0-32-14.3-32-32L0 128c0-17.7 14.3-32 32-32l64 0c17.7 0 32 14.3 32 32l0 224c0 17.7-14.3 32-32 32l-64 0z" />
     </Svg>
   );
@@ -81,18 +81,32 @@ const BookmarkSvg = () => {
 };
 interface FeedbackHeaderProps {
   closeAction: () => void;
+  actionType: string;
 }
 
-const FeedbackHeader: React.FC<FeedbackHeaderProps> = ({ closeAction }) => {
+const FeedbackHeader: React.FC<FeedbackHeaderProps> = ({
+  closeAction,
+  actionType,
+}) => {
   return (
     <View style={styles.feedbackHeader}>
-      <View style={styles.feedbackHeaderIcon}>
-        <LikeSvg style={styles.secondaryLikedBtn} />
+      <View
+        style={{
+          ...styles.feedbackHeaderIcon,
+          backgroundColor: actionType === "like" ? "#0029FF" : "red",
+        }}
+      >
+        {actionType === "like" ? (
+          <LikeSvg style={styles.secondaryLikedBtn} />
+        ) : (
+          <DislikeSvg style={styles.secondaryDislikedBtn} />
+        )}
       </View>
 
       <Text style={{ width: "80%" }}>
-        You seem to like this piece of reading. Tell me why, so we can get to
-        know you better.
+        {actionType === "like"
+          ? `You seem to like this piece of reading. Tell me why, so we can get to know you better.`
+          : `You seem to not like this piece of reading. Tell us why, so we can get to know you better.`}
       </Text>
 
       <TouchableOpacity style={styles.close} onPress={closeAction}>
@@ -117,7 +131,7 @@ export default function Article({
   const { setArticle, threadId, setThreadId, setActionType } = useContext(
     AssistantArticleContext
   );
-
+  const [actionType, setActionType] = useState<string>("");
   const updateProfile = async (clues: CluesType) => {
     const userId = await SecureStore.getItemAsync("userId");
 
@@ -194,6 +208,7 @@ export default function Article({
     });
 
     try {
+      setShowFeedbackReasoning(true);
       const result: InitializeMessageResponseType = await fetchWrapper(
         "/assistant/initialize-thread",
         {
@@ -204,7 +219,6 @@ export default function Article({
       // the value of message in the message array returns a json string, so we need to parse it even tho the fetch wrapper parses the response
       setReasoning(JSON.parse(result.messages[0].message));
       setThreadId(result.threadId);
-      setShowFeedbackReasoning(true);
     } catch (err) {
       console.log(err);
     } finally {
@@ -287,31 +301,36 @@ export default function Article({
           <ArrowSvg />
         </TouchableOpacity>
       </View>
-      {feedbackLoading && <ActivityIndicator />}
       {showFeedbackReasoning && (
         <View style={styles.feedback}>
-          <FeedbackHeader closeAction={() => setShowFeedbackReasoning(false)} />
-          <View style={styles.feedbackActions}>
-            {reasoning.Reasoning.map((reason, index) => (
+          <FeedbackHeader
+            actionType={actionType}
+            closeAction={() => setShowFeedbackReasoning(false)}
+          />
+          {feedbackLoading && <ActivityIndicator style={{ padding: 8 }} />}
+          {!feedbackLoading && (
+            <View style={styles.feedbackActions}>
+              {reasoning.Reasoning.map((reason, index) => (
+                <Action
+                  key={index}
+                  action={async () => await sendMessage(reason)}
+                >
+                  {reason}
+                </Action>
+              ))}
               <Action
-                key={index}
-                action={async () => await sendMessage(reason)}
+                // to do open the modal and provide threadID & article obj
+                key="talk-to-assistant"
+                action={() => {
+                  setIsModalVisible(true);
+                  setArticle(article.Article);
+                  setShowFeedbackReasoning(false);
+                }}
               >
-                {reason}
+                Let me chat with my assistant
               </Action>
-            ))}
-            <Action
-              // to do open the modal and provide threadID & article obj
-              key="talk-to-assistant"
-              action={() => {
-                setIsModalVisible(true);
-                setArticle(article.Article);
-                setShowFeedbackReasoning(false);
-              }}
-            >
-              Let me chat with my assistant
-            </Action>
-          </View>
+            </View>
+          )}
         </View>
       )}
     </View>
@@ -392,11 +411,17 @@ const styles = StyleSheet.create({
   },
   secondaryLikedBtn: {
     //@ts-ignore
-    //@ts-ignore
     fill: "white",
     width: 16,
     height: 16,
     backgroundColor: "#0029FF",
+  },
+  secondaryDislikedBtn: {
+    //@ts-ignore
+    fill: "white",
+    width: 16,
+    height: 16,
+    backgroundColor: "#F9325D",
   },
   dislikedBtn: {
     //@ts-ignore
@@ -425,9 +450,9 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     gap: 10,
+    alignItems: "flex-start",
   },
   feedbackHeaderIcon: {
-    backgroundColor: "#0029FF",
     display: "flex",
     alignItems: "center",
     padding: 8,
@@ -438,7 +463,6 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     gap: 12,
-
     width: "80%",
   },
   close: {
